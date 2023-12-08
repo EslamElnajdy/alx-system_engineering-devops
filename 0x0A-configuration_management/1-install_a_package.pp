3a9307a9d945cd7590bcbbf84c43eb51e9df18fc
Eslam Mohamed <eslammhamed425@gmail.com>
2023-12-08 13:41:48 +0200

    task 1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,5 @@
 # Install Flask using pip3
 package { 'flask':
-  ensure   => '2.1.0',  # Specify the desired version
-  provider => 'pip3',   # Use the pip3 provider for Python 3
+  ensure   => '2.1.0',
+  provider => 'pip3',
 }
