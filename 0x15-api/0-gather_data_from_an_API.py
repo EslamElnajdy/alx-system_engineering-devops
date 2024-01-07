4f5a09014eefddc80fa23adf99954ecd6e9275c3
Eslam Mohamed <eslammhamed425@gmail.com>
2024-01-07 03:30:31 +0200

    fix

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -4,8 +4,8 @@ Python script that, using a REST API, for a given employee ID,
 returns information about his/her TODO list progress.
 """
 
-from sys import argv
 from requests import get
+from sys import argv
 
 
 if __name__ == "__main__":
