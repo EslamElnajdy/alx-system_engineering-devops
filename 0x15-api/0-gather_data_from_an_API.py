edee4324becc9d67aaeb02225f9d132ac2d1733a
Eslam Mohamed <eslammhamed425@gmail.com>
2024-01-07 03:19:16 +0200

    initial commit

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+Python script that, using a REST API, for a given employee ID,
+returns information about his/her TODO list progress.
+"""
+
+from sys import argv
+from requests import get
+
+
+if __name__ == "__main__":
+    res1 = get(f"https://jsonplaceholder.typicode.com/users/{argv[1]}")
+    user_data = res1.json()
+    employ_name = user_data["name"]
+
+    res2 = get(f"https://jsonplaceholder.typicode.com/todos?userId={argv[1]}")
+    todo_data = res2.json()
+
+    total_tasks = len(todo_data)
+    done_task = 0
+    for task in todo_data:
+        if task["completed"]:
+            done_task += 1
+
+    print(f"Employee {employ_name} is done with tasks
+          ({done_task}/{total_tasks})")
+    for task in todo_data:
+        print(f"\t{task['title']}")
