0e72056cad056b3b2a6aaeebedcf929d2edcd83d
Eslam Mohamed <eslammhamed425@gmail.com>
2024-01-24 17:27:21 +0200

    task 0

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+queries the Reddit API and returns the number of subscribers
+"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    url = f'https://www.reddit.com/r/{subreddit}/about.json'
+    headers = {'User-Agent': 'CustomUserAgent'}
+    response = requests.get(url, headers=headers)
+
+    if response.status_code == 200:
+        try:
+            subscribers_count = response.json()['data']['subscribers']
+            return subscribers_count
+        except KeyError:
+            print("Error: invalid response format.")
+    elif response.status_code == 404:
+        return 0
+    else:
+        print(f'Error: {response.status_code}')
+        return 0
